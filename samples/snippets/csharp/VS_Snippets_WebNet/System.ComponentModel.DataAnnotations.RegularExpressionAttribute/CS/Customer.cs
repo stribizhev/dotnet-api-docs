6691edf935813e45c15d3a5f0ddc341bd7598ee9
stribizhev <stribizhev@gmail.com>
2021-12-27 23:30:22 +0100

    Update Customer.cs
    `^[a-zA-Z''-'\s]{1,40}$` = `^[a-zA-Z'\s]{1,40}$` as '-' range is just '. The existing regex made little sense and was confusing. I just suggest something that is less confusing.

--- samples/snippets/csharp/VS_Snippets_WebNet/System.ComponentModel.DataAnnotations.RegularExpressionAttribute/CS/Customer.cs
+++ samples/snippets/csharp/VS_Snippets_WebNet/System.ComponentModel.DataAnnotations.RegularExpressionAttribute/CS/Customer.cs
@@ -12,17 +12,19 @@ public class CustomerMetaData
 {
    
     //<Snippet3>
-    // Allow up to 40 uppercase and lowercase 
-    // characters. Use custom error.
-    [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", 
+    // Allow up to 40 uppercase or lowercase ASCII letters,
+    // single quotation marks, whitespaces or hyphens.
+    // Use custom error.
+    [RegularExpression(@"^[a-zA-Z'\s-]{1,40}$", 
          ErrorMessage = "Characters are not allowed.")]
     public object FirstName;
     //</Snippet3>
 
     //<Snippet2>
-    // Allow up to 40 uppercase and lowercase 
-    // characters. Use standard error.
-    [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
+    // Allow up to 40 uppercase or lowercase ASCII letters,
+    // single quotation marks, whitespaces or hyphens.
+    // Use standard error.
+    [RegularExpression(@"^[a-zA-Z'\s-]{1,40}$")]
     public object LastName;
     //</Snippet2>
 }
